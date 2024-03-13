3c9b9f70917ae61d3c2353a442620fb1b6f27e8f
Ernani12 <39600025+Ernani12@users.noreply.github.com>
2024-03-13 18:01:29 -0300

    gerando servicos

--- src/main/java/com/api/pastelaria/controller/PastelController.java
+++ src/main/java/com/api/pastelaria/controller/PastelController.java
@@ -3,10 +3,7 @@ package com.api.pastelaria.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-  
-import com.api.pastelaria.entity.Administrador;
-import com.api.pastelaria.entity.Pastel;
-import com.api.pastelaria.repository.AdministradorRepository;
+import com.api.pastelaria.model.Pastel;
 import com.api.pastelaria.repository.PastelRepository;
 
 import java.util.List;
